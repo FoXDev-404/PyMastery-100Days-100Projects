6c20619ca9d3da51ee3530cb8fb166890ce19817
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-07-19 22:34:07 +0530

    feat: Add Band Name Generator project

--- Day01-10/Day01/projectDay1.py
+++ Day01-10/Day01/projectDay1.py
@@ -1,1 +1,7 @@
-# 
+# Day 1 project: Band Name Generator
+# Band name generator project
+print("Welcome to the Band Name Generator.")
+city = input("Enter the city name you grew up in:\n")
+pet = input("Enter the name of your pet:\n")
+
+print("Your Band name is " + city + " " + pet)
