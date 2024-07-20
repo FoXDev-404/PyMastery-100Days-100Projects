8d7629e73624a6e8f52584f957aa31e0632bac4f
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-07-20 22:32:16 +0530

    primitive data types examples

---
+++ Day01-10/Day02/dataTypes.py
@@ -0,0 +1,15 @@
+# Primitive Data Type: int, float, str, bool(0,1)
+
+# String
+print("Hello"[0]) # H
+print("123" + "345") # 123345
+
+# Integer
+print(123 + 345) # 468
+print(123_456_789) # 123456789
+
+# Float
+print(3.14159) # 3.14159
+
+# Boolean
+print(True) # True
