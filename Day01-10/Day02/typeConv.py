5da06164ff23a5b237390f6664d8f901cabeed29
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-07-20 22:42:07 +0530

    type check and conversion examples

---
+++ Day01-10/Day02/typeConv.py
@@ -0,0 +1,24 @@
+# Type check and conversion
+
+# Type check
+print(type(123)) # <class 'int'>
+print(type(3.14159)) # <class 'float'>
+print(type("Hello")) # <class 'str'>
+print(type(True)) # <class 'bool'>
+
+# Type conversion
+# int to float
+print(float(123)) # 123.0
+# float to int
+print(int(3.14159)) # 3
+# float to string
+print(str(3.14159)) # 3.14159
+# string to int
+print(int("123")) # 123
+# string to float
+print(float("123.456")) # 123.456
+# string to boolean
+print(bool("True")) # True
+# boolean to string
+print(str(True)) # True
+# And so on...
