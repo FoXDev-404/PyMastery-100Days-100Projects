cb4d8c0918fb892c87e2c80e6227a008f07c5f8c
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-07-22 22:27:14 +0530

    conditional operator example in condOperator.py

---
+++ Day01-10/Day03/condOperator.py
@@ -0,0 +1,10 @@
+# Conditional operator in Python
+# Conditional operator is also known as ternary operator
+# if, else, elif are conditional statements in Python
+# Syntax: [on_true] if [expression] else [on_false]
+
+# Example 1
+a = 10
+b = 20
+min = a if a < b else b
+print(min)
