b548cdc5f365a7010c055ae4b4ef1a8ac90e517b
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-07-23 23:16:04 +0530

    feat: Update conditional operator example in condOperator.py

--- Day01-10/Day03/condOperator.py
+++ Day01-10/Day03/condOperator.py
@@ -3,8 +3,20 @@
 # if, else, elif are conditional statements in Python
 # Syntax: [on_true] if [expression] else [on_false]
 
+'''
 # Example 1
 a = 10
 b = 20
-min = a if a < b else b
-print(min)
+mini = a if a < b else b
+print(mini)
+'''
+
+# Example 2
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can vote!")
+else:
+    print("You can't vote!")
+
+# Comparison Operators >, <, >=, <=, ==, !=
