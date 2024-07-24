8081b2e03f0c904669497feac836bd48a3013926
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-07-24 21:09:45 +0530

    feat: Update nested if-else example in condOperator.py

--- Day01-10/Day03/condOperator.py
+++ Day01-10/Day03/condOperator.py
@@ -11,6 +11,7 @@ mini = a if a < b else b
 print(mini)
 '''
 
+'''
 # Example 2
 age = int(input("Enter your age: "))
 
@@ -20,3 +21,17 @@ else:
     print("You can't vote!")
 
 # Comparison Operators >, <, >=, <=, ==, !=
+'''
+
+# Example 3 [Nested if-else, elif]
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You can vote!")
+    if age >= 60:
+        print("You are a senior citizen.")
+    else:
+        print("You are not a senior citizen.")
+elif age >= 13:
+    print("You are a teenager.")
+else:
+    print("You are a kid.")
