4fdd0bca1670aba04eed1dbf29de354e3bfafbef
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-09-02 23:20:51 +0530

    refactor: Add example program using logical operators

--- Day01-10/Day03/logical_operators.py
+++ Day01-10/Day03/logical_operators.py
@@ -3,7 +3,8 @@
 # or: Returns True if one of the statements is true
 # not: Reverse the result, returns False if the result is true
 
-# Example program using normal if-else statements
+#* Example program using normal if-else statements
+'''
 def calculate_cost():
     # Ask for the user's height in centimeters
     height = int(input("Enter your height in centimeters: "))
@@ -41,3 +42,28 @@ def calculate_cost():
 
 # Start the process
 calculate_cost()
+'''
+
+#* Example program using logical operators
+def family_gathering():
+    # Ask if it's a weekend
+    is_weekend = input("Is it a weekend? (Y/N): ").strip().lower() == 'y'
+
+    # Ask if the user has finished their work
+    finished_work = input("Have you finished your work? (Y/N): ").strip().lower() == 'y'
+
+    # Ask if the user has received a special invite
+    received_invite = input("Did you receive a special invite? (Y/N): ").strip().lower() == 'y'
+
+    # Determine if the user can attend the family gathering
+    # Conditions:
+    # - It must be a weekend (is_weekend is True)
+    # - User must have finished their work (finished_work is True)
+    # - User must have received a special invite (received_invite is True)
+    if is_weekend and finished_work and (received_invite or not received_invite):
+        print("Awesome! You can attend the family gathering.")
+    else:
+        print("Oh no, you can't attend the family gathering today.")
+
+# Start the process
+family_gathering()
