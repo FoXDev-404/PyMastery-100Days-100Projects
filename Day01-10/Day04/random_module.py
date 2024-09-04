2a7df1c746ce572762a603f401f07417b69c4d34
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-09-04 23:02:50 +0530

    chore: Add random number generation using the random module

---
+++ Day01-10/Day04/random_module.py
@@ -0,0 +1,11 @@
+import random
+
+random_Int = random.randint(1, 6)
+
+print(random_Int)
+
+random_float = random.random()# 0.0 <= random_float < 1.0
+print(random_float)
+
+random_float1 = random.random() * 100 # 0.0 <= random_float < 100.0
+print(random_float1)
