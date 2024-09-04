6c17dc109cdedd9b0a651521939d6f42bcba4fea
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-09-04 23:10:25 +0530

    chore: Update random_module.py to generate a random number between 0 and 1 and using conditional operators created a Head/Tail game

--- Day01-10/Day04/random_module.py
+++ Day01-10/Day04/random_module.py
@@ -1,11 +1,20 @@
 import random
 
-random_Int = random.randint(1, 6)
+# random_Int = random.randint(1, 6)
+#
+# print(random_Int)
+#
+# random_float = random.random()# 0.0 <= random_float < 1.0
+# print(random_float)
+#
+# random_float1 = random.random() * 100 # 0.0 <= random_float < 100.0
+# print(random_float1)
+#
+# random_float2 = random.uniform(1, 100) # 1.0 <= random_float < 100.0
+# print(random_float2)
 
-print(random_Int)
-
-random_float = random.random()# 0.0 <= random_float < 1.0
-print(random_float)
-
-random_float1 = random.random() * 100 # 0.0 <= random_float < 100.0
-print(random_float1)
+random_num = random.randint(0, 1)
+if random_num == 0:
+    print("Heads")
+else:
+    print("Tails")
