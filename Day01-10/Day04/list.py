78b78dd946ce20ceffb0eb4f68df1412b7f197e7
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2024-09-06 22:52:38 +0530

    feat: Add examples of accessing and slicing list items in list.py

--- Day01-10/Day04/list.py
+++ Day01-10/Day04/list.py
@@ -2,24 +2,24 @@
 # List is defined by square brackets [].
 # List elements are separated by commas.
 
-# List creation
+#* List creation
 # Empty list
 empty_list = []
 print(empty_list)  # []
 
-# List with elements
+#* List with elements
 fruits = ['apple', 'banana', 'cherry']
 print(fruits)  # ['apple', 'banana', 'cherry']
 
-# List with mixed data types
+#* List with mixed data types
 mixed_list = [1, 'apple', 2.5, True]
 print(mixed_list)  # [1, 'apple', 2.5, True]
 
-# List with nested lists
+#* List with nested lists
 nested_list = [1, [2, 3], [4, 5, 6]]
 print(nested_list)  # [1, [2, 3], [4, 5, 6]]
 
-# List with list comprehension
+#* List with list comprehension
 squares = [x**2 for x in range(1, 6)]
 print(squares)  # [1, 4, 9, 16, 25]
 
@@ -29,7 +29,15 @@ print(squares)  # [1, 4, 9, 16, 25]
 # Negative index starts from -1.
 # List items can be accessed using slicing.
 
-# Accessing list items
+#* Accessing list items
 fruits = ['apple', 'banana', 'cherry']
 print(fruits[0])  # apple # Positive index
 print(fruits[-1])  # cherry # Negative index
+
+#* Slicing list
+# Start index is inclusive, end index is exclusive
+print(fruits[1:])  # ['banana', 'cherry']
+print(fruits[:2])  # ['apple', 'banana']
+print(fruits[1:2])  # ['banana']
+print(fruits[-2:])  # ['banana', 'cherry']
+print(fruits[:-1])  # ['apple', 'banana']
