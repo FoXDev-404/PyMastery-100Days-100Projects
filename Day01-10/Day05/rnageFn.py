19bcaa5a5659a940ff67fe2bf3678c9e4e9fa928
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-03-09 22:59:29 +0530

    Add range function implementation and FizzBuzz logic in projectDay5.py and rnageFn.py

---
+++ Day01-10/Day05/rnageFn.py
@@ -0,0 +1,21 @@
+# Range function with loop
+
+for number in range(1,101):
+    print(number)
+    
+# Sum of 1 to 100
+sum = 0
+for number in range(1,101):
+    sum += number
+print(sum)
+
+
+for numbers in range(1,101):
+    if numbers%3 ==0 and numbers%5==0:
+        print("FizzBuzz")
+    elif numbers%3==0:
+        print("Fizz")
+    elif numbers%5==0:
+        print("Buzz")
+    else:
+        print(numbers)
