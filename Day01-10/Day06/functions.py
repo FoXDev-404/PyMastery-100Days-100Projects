f2c411c810f7a54ac239b4bca8ca4f75604a2380
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-03-15 13:01:08 +0530

    Add function definition example and whileLoop.py file create

---
+++ Day01-10/Day06/functions.py
@@ -0,0 +1,17 @@
+'''
+A function is a block of code which only runs when it is called.
+
+You can pass data, known as parameters, into a function.
+
+A function can return data as a result.
+
+'''
+
+# In Python a function is defined using the "def" keyword
+def my_function():
+  print("This is a function")
+
+my_function() # This will call the function and print the message
+
+string_length = len("This is a len() function example")
+print(string_length)
