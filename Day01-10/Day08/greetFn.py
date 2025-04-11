8175fc942cf68f78ad44e6860d1dbde771b5eb53
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-04-11 22:38:23 +0530

    Refactor greeting functions to improve structure and clarity

--- Day01-10/Day08/greetFn.py
+++ Day01-10/Day08/greetFn.py
@@ -5,10 +5,18 @@
 
 # greet()
 
-# function with name 
-def greet_with_name(name):
-    print(f"Namaste {name}")
-    print(f"Hello {name}")
-    print(f"Good Morning {name}")
+# # function with name 
+# def greet_with_name(name):
+#     print(f"Namaste {name}")
+#     print(f"Hello {name}")
+#     print(f"Good Morning {name}")
     
-greet_with_name("Raj")
+# greet_with_name("Raj")
+
+# Function with more then 1 input
+def greet_with(name,location):
+    print(f"Good morning {name} from {location}")
+    
+greet_with("Raj", "India")
+greet_with("India", "Raj") # Positional argument
+greet_with(location="India", name="Raj") # Keyword argument
