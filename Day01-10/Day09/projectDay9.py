12624ca458f25538eb81cab777e74dfca17a1e05
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-04-16 23:43:50 +0530

    Implement secret auction program with bidding functionality

--- Day01-10/Day09/projectDay9.py
+++ Day01-10/Day09/projectDay9.py
@@ -1,2 +1,18 @@
 # Secret auction program project
 
+bid_dict = {}
+
+
+while True:
+    add_more = input(f"Any one else want to bid 'yes' or 'no': ").lower()
+        
+    if add_more == "yes":
+        user = input("Enter Your name: ")
+        bid_price = int(input("Enter the bid amount: $"))
+        bid_dict[user] = bid_price
+        # print(bid_dict)
+    elif add_more == "no":
+        break
+    
+max_bid = max(bid_dict,key=bid_dict.get)
+print(f"Largest bid is ${bid_dict[max_bid]} by '{max_bid}'")
