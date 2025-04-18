534abcb3523e271b558bfb18def2af813a94b0e3
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-04-18 20:32:57 +0530

    Fucntion with Outputs

--- Day01-10/Day10/outputFun.py
+++ Day01-10/Day10/outputFun.py
@@ -1,1 +1,10 @@
 ## Fucntion with Outputs
+
+def foramt_name():
+    f_name = str(input("Enter Your First Name: ")).title()
+    l_name = str(input("Enter Your Last Name: ")).title()
+    
+    # print(f"{f_name} {l_name}")
+    return f"{f_name} {l_name}"
+    
+print(foramt_name())
