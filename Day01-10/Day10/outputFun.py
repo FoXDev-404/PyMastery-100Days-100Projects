c0f09ba9c3e08fb4618e71b20a91ca0a7039df2c
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-04-18 20:39:30 +0530

    Fix formatting function to return a result message and handle invalid inputs

--- Day01-10/Day10/outputFun.py
+++ Day01-10/Day10/outputFun.py
@@ -4,7 +4,9 @@ def foramt_name():
     f_name = str(input("Enter Your First Name: ")).title()
     l_name = str(input("Enter Your Last Name: ")).title()
     
+    if f_name == "" or l_name == "":
+        return "You did not provide valid inputs"
     # print(f"{f_name} {l_name}")
-    return f"{f_name} {l_name}"
+    return f"Result: {f_name} {l_name}"
     
 print(foramt_name())
