2f1fa381803261a7ad510c3afcf99a39ff7857b9
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-04-24 00:10:48 +0530

    Fix comment typo and add verification print statements in use_print.py

--- Day11-20/Day13/use_print.py
+++ Day11-20/Day13/use_print.py
@@ -2,7 +2,10 @@
 
 word_per_page = 0
 pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: ")) # Used an "==" comprision opeator not assignment operator
+# word_per_page == int(input("Number of words per page: ")) # Used an "==" comparison operator, not assignment operator
 word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
+
+# print(f"Pages = {pages}")          # To verify
+# print(f"Words per page = {word_per_page}")  # To verify
 print(total_words)
