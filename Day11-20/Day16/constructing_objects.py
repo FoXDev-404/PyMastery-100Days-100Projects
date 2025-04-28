a9b0149cb3b599933d69572652ff29ede8cf4fe8
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-04-28 22:44:13 +0530

    Constructing Objects and Accessing their Attributes using the example of add turtle graphics implementation

--- Day11-20/Day16/constructing_objects.py
+++ Day11-20/Day16/constructing_objects.py
@@ -1,1 +1,11 @@
-# Constructiong Objects and Accessing their Attributes and Methods
+# Constructing Objects and Accessing their Attributes and Methods
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("DarkOrchid2")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
