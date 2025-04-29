b02ff4f7fca571fcd423cdc4e2d7ec24299707e7
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-04-29 23:46:33 +0530

    Add User class implementation with initialization and follower count

--- Day11-20/Day17/class.py
+++ Day11-20/Day17/class.py
@@ -1,1 +1,12 @@
 # How to create class
+class User:
+
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+    
+user_1 = User("001", "Rajpal")
+user_2 = User("002", "Nishad")
+
+print(user_1.followers)
