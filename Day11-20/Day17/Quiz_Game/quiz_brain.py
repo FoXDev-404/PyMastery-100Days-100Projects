274959d4d9d73a1421a2f693f5d5d89bf926bdc8
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-02 18:42:29 +0530

    Refactor quiz game logic to ensure questions are presented in a loop until all are answered

--- Day11-20/Day17/Quiz_Game/quiz_brain.py
+++ Day11-20/Day17/Quiz_Game/quiz_brain.py
@@ -4,7 +4,18 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
         
+    def still_has_questions(self):
+        return self.question_number <len(self.question_list)
+        
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{current_question.text}: {current_question.text} (True/False): ")
+        user_answer = input(f"Q.{current_question.text}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+        
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
