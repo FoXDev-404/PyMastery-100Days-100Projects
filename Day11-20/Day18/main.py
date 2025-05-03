f58a014a43348075faee226aa3d1edb8e4ea900a
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-03 21:50:13 +0530

    Refactor Turtle Graphics code to improve readability and structure square drawing logic

--- Day11-20/Day18/main.py
+++ Day11-20/Day18/main.py
@@ -3,12 +3,14 @@
 
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("tomato3")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+tim = Turtle()
+tim.shape("turtle")
+tim.color("tomato3")
 
+# Draw a Square
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
 
 
 
