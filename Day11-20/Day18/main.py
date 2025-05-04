9caf0b600ea35f35e7ddb4a4579d64c57d9cec43
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-04 18:42:11 +0530

    Refactor random walk functionality to improve color selection and code organization

--- Day11-20/Day18/main.py
+++ Day11-20/Day18/main.py
@@ -22,7 +22,7 @@
 
 ## =============================================================== ##
 
-# TODO : Drawing a tringle(120 deg), square(90 deg), pentagon(72 deg), hexagon(60 deg),
+# TODO: Drawing a tringle(120 deg), square(90 deg), pentagon(72 deg), hexagon(60 deg),
 # heptagon(51.42857142857143 deg), octagon(45 deg), nonagon(40) and decagon(36)
 # from turtle import Turtle
 # import random
@@ -53,27 +53,34 @@
 
 ## =============================================================== ##
 
-# TODO : Draw a Random Walk
+# TODO: Draw a Random Walk
 import random
+import turtle
 from turtle import Turtle, Screen
 
 tim = Turtle()
+turtle.colormode(255)
 tim.pensize(10)
 tim.speed("fastest")
 
-colors = ['red', 'blue', 'green', 'purple', 'yellow', 'orange', 'pink', 'cyan', 'magenta', 'brown']
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 
 # # Option 1: Turn only by 90° increments
 # directions = [0, 90, 180, 270]  # Forward, Right, Backward, Left
 # for _ in range(200):
-#     tim.color(random.choice(colors))
+#     tim.color(random_color())
 #     tim.right(random.choice(directions))
 #     tim.forward(30)
 
 # Option 2: Use setheading() with absolute directions
 directions = [0, 90, 180, 270]  # East, North, West, South
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.setheading(random.choice(directions))
     tim.forward(30)
 
