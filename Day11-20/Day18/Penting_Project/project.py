6ac2275ec2837602de1a797771e7cf80aad48c96
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-05 21:52:27 +0530

    update color extraction to use absolute path with increased color count

--- Day11-20/Day18/Penting_Project/project.py
+++ Day11-20/Day18/Penting_Project/project.py
@@ -1,10 +1,15 @@
-# TODO: Recareating the Hirst spot penting
+# TODO: Recreating the Hirst spot painting
 
 import colorgram
+import os
 
-
-colors = colorgram.extract('./image.png', 6)
+# Use absolute path to be certain of image location
+image_path = os.path.join(os.path.dirname(__file__), "image.png")
+colors = colorgram.extract(image_path, 20)
 
 first_color = colors[0]
 rgb = first_color.rgb
-print(colors)
+print(rgb)
+
+rgb_colors = [(color.rgb.r, color.rgb.g, color.rgb.b) for color in colors]
+print(rgb_colors)
