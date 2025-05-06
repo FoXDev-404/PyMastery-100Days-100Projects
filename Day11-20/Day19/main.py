a1f4c23c370c66ecbc9a63b0430aa3f70f1ba2e3
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-06 23:03:10 +0530

    update key bindings for turtle movement controls

--- Day11-20/Day19/main.py
+++ Day11-20/Day19/main.py
@@ -3,9 +3,27 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
-def move_farwards():
+def move_forwards():
     tim.forward(18)
+    
+def move_backwards():
+    tim.backward(10)
+    
+def turn_left():
+    new_heading = tim.heading()+10
+    tim.setheading(new_heading)
+def turn_right():
+    new_heading = tim.heading()-10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.home()
 
 screen.listen()
-screen.onkey(key="space", fun=move_farwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
 screen.exitonclick()
