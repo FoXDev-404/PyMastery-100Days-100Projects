e860bb34ecd827549a8f60fba6580155fcce70a3
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-07 21:02:10 +0530

    Add complete implementation of Turtle Racing Game with user betting input

--- Day11-20/Day19/turtle_race.py
+++ Day11-20/Day19/turtle_race.py
@@ -1,1 +1,17 @@
 # Turtle Racing Game
+
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a Color")
+
+
+tim =Turtle(shape="turtle")
+
+tim.goto(x=-230, y=-100)
+
+
+screen.exitonclick()
