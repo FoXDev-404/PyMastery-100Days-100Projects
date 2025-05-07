b95513b6c462143a377db39b0650731bc00332e5
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-07 22:08:05 +0530

    Refactor turtle race setup by organizing turtle creation and improving code structure

--- Day11-20/Day19/turtle_race.py
+++ Day11-20/Day19/turtle_race.py
@@ -1,17 +1,24 @@
-# Turtle Racing Game
-
 from turtle import Turtle, Screen
 import random
 
+# Set up screen
 screen = Screen()
 screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:")
 
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a Color")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-100, -60, -20, 20, 60, 100]
 
+all_turtles = []
 
-tim =Turtle(shape="turtle")
-
-tim.goto(x=-230, y=-100)
-
+# Create turtles and set starting positions
+for turtle_index in range(6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+    
+    
 
 screen.exitonclick()
