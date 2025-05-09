7238f1bfe0dd4b7e7c6bfb92673daadf1f2cb52f
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-09 21:31:39 +0530

    Add initial setup for Snake Game with screen configuration and starting position

--- Day11-20/Day20/main.py
+++ Day11-20/Day20/main.py
@@ -1,1 +1,30 @@
 # === Snake Game: Part 1 - Animation and Coordinates ===
+
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+starting_position = [(0,0), (-20,0), (-40,0)]
+
+for positon in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(positon)
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
