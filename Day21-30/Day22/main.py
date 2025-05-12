0cebe3b75040fec969dc27bd98e96bab243b4bd6
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-12 21:53:31 +0530

    Refactor PONG Game to use Paddle class for paddle functionality

--- Day21-30/Day22/main.py
+++ Day21-30/Day22/main.py
@@ -1,6 +1,6 @@
 # PONG Game
 from turtle import Screen, Turtle
-
+from paddle import Paddle
 
 screen = Screen()
 screen.title("PONG Game")
@@ -8,28 +8,15 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)  # Turns off the screen updates
 
-# Paddle
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-
-def go_up():
-    y = paddle.ycor()
-    y += 20
-    paddle.sety(y)
-
-def go_down():
-    y = paddle.ycor()
-    y -= 20
-    paddle.sety(y)
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
