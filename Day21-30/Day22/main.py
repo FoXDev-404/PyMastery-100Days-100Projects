82f83bdb705cb13c9a0f62ecf5452ca45990908d
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-12 21:41:53 +0530

    Add paddle functionality and controls to PONG Game

--- Day21-30/Day22/main.py
+++ Day21-30/Day22/main.py
@@ -1,13 +1,38 @@
 # PONG Game
-from turtle import Screen
+from turtle import Screen, Turtle
 
 
 screen = Screen()
 screen.title("PONG Game")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
+screen.tracer(0)  # Turns off the screen updates
+
+# Paddle
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
 
 
+def go_up():
+    y = paddle.ycor()
+    y += 20
+    paddle.sety(y)
 
+def go_down():
+    y = paddle.ycor()
+    y -= 20
+    paddle.sety(y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()  # Update the screen
 
 screen.exitonclick()
