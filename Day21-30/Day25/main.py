775f84bb80d6ab508bf45b709c7617a99b4cd195
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-15 23:55:06 +0530

    refactor: Replace CSV reading logic with pandas for improved data handling

--- Day21-30/Day25/main.py
+++ Day21-30/Day25/main.py
@@ -2,12 +2,17 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
+# import csv
 
-with open('weather_data.csv') as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data:
-        if row[1].strip() != 'temp':
-            temperature.append(int(row[1]))
-    print(temperature)
+# with open('weather_data.csv') as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1].strip() != 'temp':
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
+import pandas
+
+data = pandas.read_csv('weather_data.csv')
+print(data)
