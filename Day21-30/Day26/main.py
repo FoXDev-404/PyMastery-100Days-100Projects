ccad4208d3c76137c8962947e591efea856ee143
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-18 23:44:42 +0530

    Add examples of list comprehension with string manipulation in main.py

--- Day21-30/Day26/main.py
+++ Day21-30/Day26/main.py
@@ -5,3 +5,12 @@ print(squares)  # Output: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 # List comprehension with condition
 evens = [x for x in range(10) if x % 2 == 0]
 print(evens)  # Output: [0, 2, 4, 6, 8]
+
+# List comprehension with string manipulation
+names = ["Alex", "Beth", "Carolina", "Dave", "Elanor", "Freddie"]
+
+short_names = [name for name in names if len(name) < 5]
+print(short_names)  # Output: ['Alex', 'Beth', 'Dave']
+
+long_names = [name.upper() for name in names if len(name) > 5]
+print(long_names)
