6a79d8ada333c92bf3a16f8f9610f1fee0041cb0
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-20 22:01:41 +0530

    Add Car class implementation utilizing **kwargs in the constructor

--- Day21-30/Day27/playground.py
+++ Day21-30/Day27/playground.py
@@ -15,3 +15,17 @@ def calculate(n,**kwargs):
     print(f"Result: {n}")
 calculate(2, add=3, multiply=5)
 # Output: Result: 25
+
+# ====== Defining a class that uses **kwargs in the constructor ====== #
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan", model="GT-R", color="Red", seats=2)
+print(my_car.make)
+print(my_car.model)
+print(my_car.color)
+print(my_car.seats)
