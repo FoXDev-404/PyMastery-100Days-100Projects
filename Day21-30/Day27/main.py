f0163b3972906d320e6daa92e5a374307489e109
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-20 23:56:04 +0530

    Refactor Tkinter GUI code to streamline imports and enhance button functionality

--- Day21-30/Day27/main.py
+++ Day21-30/Day27/main.py
@@ -1,14 +1,31 @@
 # Graphical User Interface (GUI) using Tkinter
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-label = tkinter.Label(text="Hello, Tkinter!", font=("Arial", 24, "italic"))
+label = Label(text="Hello, Tkinter!", font=("Arial", 24, "italic"))
 label.pack()
 
+# Button
+def button_clicked():
+    label.config(text="Button Clicked!") # Update label text when button is clicked
+
+# button = Button(text="Click Me", font=("Arial", 14), command=button_clicked)
+# button.pack()
+
+# Entry
+input = Entry(width=30)
+input.pack()
+input.get() # Get the text from the entry field
+def get_input():
+    user_input = input.get()
+    label.config(text=user_input)
+
+button = Button(text="Submit", font=("Arial", 14), command=get_input)
+button.pack()
 
 
 
