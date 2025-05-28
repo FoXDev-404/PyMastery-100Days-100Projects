cba9ca96c9e7bc222c1399633864ad486f3f4f07
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-28 22:50:46 +0530

    Add error handling examples to main.py; include common exceptions such as FileNotFoundError, KeyError, IndexError, and TypeError

---
+++ Day21-30/Day30/main.py
@@ -0,0 +1,16 @@
+# FileNotFoundError
+# with open("a_file.txt", "r") as file:
+#     file.read()
+
+# KeyError
+# my_dict = {"name": "Alice"}
+# print(my_dict["age"])
+
+# IndexError
+# my_list = [1, 2, 3]
+# print(my_list[5])
+
+# TypeError
+# def add_numbers(a, b):
+#     return a + b
+# print(add_numbers(5, "10"))
