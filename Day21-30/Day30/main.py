2824dd45f2df6d450ce49ffe13f6ff6c23293708
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-05-28 22:59:52 +0530

    Implement error handling in main.py; add handling for FileNotFoundError and KeyError, and create a_file.txt if not found.

--- Day21-30/Day30/main.py
+++ Day21-30/Day30/main.py
@@ -2,6 +2,23 @@
 # with open("a_file.txt", "r") as file:
 #     file.read()
 
+# Fix the code to handle the error
+try:
+    file = open("a_file.txt", "r") # Attempt to open a file that may not exist
+    a_dict = {"name": "Alice", "age": 30} # Example dictionary
+    print(a_dict["age"]) # Accessing a key that exists
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("This is a new file created because the original was not found.")
+except KeyError as error_message:
+    print(f"KeyError: {error_message} - The key does not exist in the dictionary.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("FIle was closed.")
+
 # KeyError
 # my_dict = {"name": "Alice"}
 # print(my_dict["age"])
