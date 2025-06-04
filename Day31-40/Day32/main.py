19cb03c9b29817787f32d6bd6515c86dba06b2d5
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-06-04 23:08:27 +0530

    Refactor email sending logic to improve clarity and maintain functionality, while commenting out unused code for future reference.

--- Day31-40/Day32/main.py
+++ Day31-40/Day32/main.py
@@ -1,3 +1,35 @@
+import smtplib
+import datetime as dt
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+now = dt.datetime.now()
+year = now.weekday()  # Monday is 0, Sunday is 6
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+        
+    print(quote)
+    
+    EMAIL = os.getenv("EMAIL")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+    EMAIL_RECIPIENT = os.getenv("EMAIL_RECIPIENT")
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=EMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL_RECIPIENT,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
+
+
 # import smtplib
 # import os
 # from dotenv import load_dotenv
@@ -20,13 +52,13 @@
 # print("Email sent successfully! 💌")
 
 
-import datetime as dt
+# import datetime as dt
 
-now = dt.datetime.now()  # Get the current date and time
-year = now.year  # Extract the year
-month = now.month  # Extract the month
-day = now.day  # Extract the day
-print(f"Year: {year}, Month: {month}, Day: {day}")
+# now = dt.datetime.now()  # Get the current date and time
+# year = now.year  # Extract the year
+# month = now.month  # Extract the month
+# day = now.day  # Extract the day
+# print(f"Year: {year}, Month: {month}, Day: {day}")
 
-date_of_birth = dt.datetime(year=2004, month=11, day=6)
-print(f"Date of Birth: {date_of_birth}")
+# date_of_birth = dt.datetime(year=2004, month=11, day=6)
+# print(f"Date of Birth: {date_of_birth}")
