ed0c26bd3284603888f0661bf9d990784ade01a9
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-06-12 20:15:05 +0530

    Refactor user parameters in Pixela API integration for improved readability and maintainability

--- Day31-40/Day37/main.py
+++ Day31-40/Day37/main.py
@@ -2,12 +2,35 @@ import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
+USERNAME = "foxdev"
+TOKEN = "asdaskj4kljk8dkj"
+
 user_params = {
-    "token": "iskdopgsbr48dm",
-    "username": "foxdev",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
 # response = requests.post(pixela_endpoint, json=user_params)
 # print(response.text)
+
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "hours",
+    "type": "float",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
+
+# Link to the graph: https://pixela.v1/users/foxdev/graphs/graph1.html
