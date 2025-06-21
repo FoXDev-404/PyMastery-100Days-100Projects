b0188bbb39ce04ddba763ae90137a322c8b14feb
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-06-21 18:00:40 +0530

    Add functionality to extract and print all anchor tags from the HTML using BeautifulSoup

--- Day41-50/Day45/main.py
+++ Day41-50/Day45/main.py
@@ -12,3 +12,14 @@ soup = BeautifulSoup(content, "html.parser")
 # print(soup.prettify())
 
 # print(soup.a)
+
+all_anchor_tags = (soup.find_all(name="a"))
+# print(all_anchor_tags)
+
+# for tag in all_anchor_tags:
+      # print(tag.getText())
+#     print(tag.get("href"))
+
+
+# print(soup.find_all(name="h1", id="name"))
+ 
