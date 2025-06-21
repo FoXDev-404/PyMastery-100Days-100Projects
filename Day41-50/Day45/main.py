5e5e432b7ff072519457a8903da52e5685464b53
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-06-21 18:34:14 +0530

    Add venv/ to .gitignore to exclude virtual environment files

--- Day41-50/Day45/main.py
+++ Day41-50/Day45/main.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 # import lxml
 
 
-with open("website.html") as file:
-    content = file.read()
+# with open("website.html") as file:
+#     content = file.read()
 
-soup = BeautifulSoup(content, "html.parser")
+# soup = BeautifulSoup(content, "html.parser")
 # print(soup.title)
 # print(soup.title.string)
 
@@ -13,7 +13,7 @@ soup = BeautifulSoup(content, "html.parser")
 
 # print(soup.a)
 
-all_anchor_tags = (soup.find_all(name="a"))
+# all_anchor_tags = (soup.find_all(name="a"))
 # print(all_anchor_tags)
 
 # for tag in all_anchor_tags:
@@ -22,4 +22,25 @@ all_anchor_tags = (soup.find_all(name="a"))
 
 
 # print(soup.find_all(name="h1", id="name"))
- 
+
+
+
+# ===========Test in index.html website===============
+with open("./index.html") as file:
+    content = file.read()
+    
+soup = BeautifulSoup(content, "html.parser")
+
+# fruit = soup.select_one(selector="#fruits")
+# print(fruit)
+#
+# veg = soup.select(".veg")
+# print(veg)
+
+# Parse table rows
+print("\nUser Table:")
+rows = soup.select("#user-table tbody tr")
+for row in rows:
+    cols = row.find_all("td")
+    name, email, age = [col.text for col in cols]
+    print(f"  - {name} ({email}) is {age} years old.")
