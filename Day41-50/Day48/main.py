c65b40305f340c339fc05352ec29c60e708aff21
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-06-30 03:17:55 +0530

    Add script to open Amazon homepage using Selenium with detached Chrome

---
+++ Day41-50/Day48/main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+# Keep Chrome open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://amazon.in")
+
+
+# driver.close()
+driver.quit()
