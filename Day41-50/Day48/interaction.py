7778698d4ad232c2c41e291e4aaf4e8fa5b77ce7
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-07-16 23:42:59 +0530

    Enhance interaction script to include search functionality and element retrieval from Wikipedia

--- Day41-50/Day48/interaction.py
+++ Day41-50/Day48/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # Keep Chrome open after process ends
 chrome_options = webdriver.ChromeOptions()
@@ -8,8 +9,21 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-no_of_articles = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/ul/li[2]/a[1]')
-print(f"{no_of_articles.text} articles in English")
+article_count = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/ul/li[2]/a[1]')
+# article_count.click()
+
+# Find elements by link Text
+all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+# Find elements by name <Search>
+search = driver.find_element(By.CLASS_NAME, value="search-toggle").click()
+search_input = driver.find_element(By.CLASS_NAME, value="cdx-text-input__input")
+
+# Sending keyboard input to Selenium
+search_input.send_keys("Python", Keys.ENTER)
+
+
 
 # driver.close()
-driver.quit()
+# driver.quit()
