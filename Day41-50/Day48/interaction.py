762bee1cc7cb05a71cfc8b2423e9bb0bf9abdbbc
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-07-17 00:02:21 +0530

    Update interaction script to automate form submission on secure-retreat website

--- Day41-50/Day48/interaction.py
+++ Day41-50/Day48/interaction.py
@@ -7,23 +7,36 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+#
+# article_count = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/ul/li[2]/a[1]')
+# # article_count.click()
+#
+# # Find elements by link Text
+# all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# # all_portals.click()
+#
+# # Find elements by name <Search>
+# search = driver.find_element(By.CLASS_NAME, value="search-toggle").click()
+# search_input = driver.find_element(By.CLASS_NAME, value="cdx-text-input__input")
+#
+# # Sending keyboard input to Selenium
+# search_input.send_keys("Python", Keys.ENTER)
 
-article_count = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/ul/li[2]/a[1]')
-# article_count.click()
+#===== Challenge ======
+driver.get("https://secure-retreat-92358.herokuapp.com")
 
-# Find elements by link Text
-all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
-# all_portals.click()
+first_name = driver.find_element(By.NAME, "fName")
+last_name = driver.find_element(By.NAME, "lName")
+email = driver.find_element(By.NAME, "email")
 
-# Find elements by name <Search>
-search = driver.find_element(By.CLASS_NAME, value="search-toggle").click()
-search_input = driver.find_element(By.CLASS_NAME, value="cdx-text-input__input")
+# FIll out the form
+first_name.send_keys("Rajpal")
+last_name.send_keys("Nishad")
+email.send_keys("rajpal010304@gmail.com")
 
-# Sending keyboard input to Selenium
-search_input.send_keys("Python", Keys.ENTER)
-
-
+button = driver.find_element(By.CLASS_NAME, value="btn")
+button.click()
 
 # driver.close()
 # driver.quit()
