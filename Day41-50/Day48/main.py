834a70a883ea116f47cd4f7d5ce08b848d3f7ac2
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-07-16 22:10:57 +0530

    Refactor Selenium script to simplify Chrome driver initialization and update price retrieval logic

--- Day41-50/Day48/main.py
+++ Day41-50/Day48/main.py
@@ -1,32 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-# Keeps Chrome open after execution
-chrome_options = Options()
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-# Initialize driver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://a.co/d/e26OpX7")
 
-def test_eight_components():
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-    assert driver.title == "Web form"
+price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+print(f"The price is {price_dollar.text}.{price_cents.text}")
 
-    driver.implicitly_wait(5)
 
-    text_box = driver.find_element(by=By.NAME, value="my-text")
-    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-    text_box.send_keys("Selenium")
-    submit_button.click()
-
-    message = driver.find_element(by=By.ID, value="message")
-    assert message.text == "Received!"
-
-    driver.quit()
-
-if __name__ == "__main__":
-    test_eight_components()
+# driver.close()
+driver.quit()
