17dafa468ba6f9bd06c2bb6bf1c4eed79195bbf4
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-08-02 00:02:53 +0530

    Rename index.html to guess.html and update title; add age and gender guessing feature

--- Day51-60/Day57/main.py
+++ Day51-60/Day57/main.py
@@ -2,15 +2,24 @@
 from flask import Flask, render_template
 from datetime import datetime
 import random
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    num = random.randint(1, 100)
+    num = random.randint(1, 10)
     year = datetime.now().year
     return render_template('index.html', num=num, year=year)
+
+@app.route("/guess/<name>")
+def guess(name):
+    gender_response = requests.get(f"https://api.genderize.io?name={name}")
+    age_response = requests.get(f"https://api.agify.io?name={name}")
+    age = age_response.json()['age']
+    gender = gender_response.json()['gender']
+    return render_template('guess.html', name=name, age=age, gender=gender)
 
 
 if __name__ == "__main__":
