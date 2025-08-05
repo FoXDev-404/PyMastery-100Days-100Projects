e96ad46fce10f957cf9bed595ba85a68419b028f
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-08-06 00:50:11 +0530

    Add initial Flask application with login form and index template

---
+++ Day51-60/Day60/html-forms/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/login', methods=['POST'])
+def receive_data():
+    username = request.form['username']
+    password = request.form['password']
+    return f"<h1>Welcome {username}!</h1><h1>Password is {password}</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
