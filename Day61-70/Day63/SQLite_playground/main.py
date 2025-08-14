d9e9295d283ac3ca51f7036c92ac12baee1ec19c
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-08-15 02:35:44 +0530

    feat: Implement CRUD operations for book records in the SQLite database

--- Day61-70/Day63/SQLite_playground/main.py
+++ Day61-70/Day63/SQLite_playground/main.py
@@ -57,3 +57,32 @@ with app.app_context():
     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
     db.session.add(new_book)
     db.session.commit()
+
+# Read all Records
+with app.app_context():
+    result = db.session.execute(db.select(Book).order_by(Book.title))
+    all_books = result.scalars()
+
+# Read A Particular Records By Query
+with app.app_context():
+    book = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+    # To get a single element we can use scalar() instead of scalars().
+
+# Update A Record By PRIMARY KEYbook_id = 1
+with app.app_context():
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_update = db.get_or_404(Book, book_id)
+    book_to_update.title = "Harry Potter and the Goblet of Fire"
+    db.session.commit()
+# Flask-SQLAlchemy also has some handy extra query methods like get_or_404 that we can use.
+# Since Flask-SQLAlchemy version 3.0 the previous query methods like Book.
+# query.get have been deprecated
+
+# Delete A Particular Record By PRIMARY KEYbook_id = 1
+with app.app_context():
+    book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    # or book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+# You can also delete by querying for a particular value e.g. by title or one of the other properties.
+# Again, the get_or_404() method is quite handy.
