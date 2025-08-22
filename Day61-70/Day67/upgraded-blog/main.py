e2258c6a3c17e0676fbc126bc8f7816e089101bd
Rajpal Nishad <114016588+FoXDev-404@users.noreply.github.com>
2025-08-23 02:50:26 +0530

    feat: Implement new post creation functionality with CKEditor integration

--- Day61-70/Day67/upgraded-blog/main.py
+++ Day61-70/Day67/upgraded-blog/main.py
@@ -9,9 +9,44 @@ from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# Use the latest CKEditor from CDN
+app.config['CKEDITOR_PKG_TYPE'] = 'standard'
+app.config['CKEDITOR_CDN'] = 'https://cdn.ckeditor.com/4.25.1/standard/ckeditor.js'
 Bootstrap5(app)
+ckeditor = CKEditor(app)
+# TODO: add_new_post() to create a new blog post
+
+# WTForm for creating a new post
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+# Route to create a new post
+from flask import request
+@app.route('/new-post', methods=["GET", "POST"])
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            author=form.author.data,
+            img_url=form.img_url.data,
+            body=form.body.data,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    return render_template("make-post.html", form=form)
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
